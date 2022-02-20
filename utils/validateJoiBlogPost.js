4aaebc272a5fe3d1c8a3cb52c5d9d30d99dbde98
MIkaela Braga <bragamikaela@gmail.com>
2022-02-20 19:50:18 -0300

    Cria validaçoes de input com o Joi

---
+++ utils/validateJoiBlogPost.js
@@ -0,0 +1,15 @@
+const Joi = require('joi');
+
+const blogPostSchema = Joi.object({
+  title: Joi.string().required(),
+  content: Joi.string().required(),
+  categoryId: Joi.array().required(),
+});
+
+const validateJoiBlogPost = (object) => {
+  const { error } = blogPostSchema.validate(object);
+
+  if (error) throw error;
+};
+
+module.exports = { validateJoiBlogPost };
